4c9b2869b2120ac93a70defac77e6e711d982c5b
Javier Lurgo (ntbk) <jlurgo@gmail.com>
2013-08-22 05:30:09 -0300

    4_login: transicion hacia la segunda pagina

--- PantallaExplorador.js
+++ PantallaExplorador.js
@@ -4,20 +4,22 @@ var PantallaExplorador = function(opt){
 };
 
 PantallaExplorador.prototype.start = function(){
-    this.ui = $("#pantalla_explorador");
+    this.ui = $("#pantalla_explorador");    
+    $.mobile.changePage (this.ui);
+    
     this.encabezado = this.ui.find("#encabezado");
     this.encabezado.text(this.o.usuario.nombre);
     var pos_obelisco = new google.maps.LatLng(-34.603683,-58.381569);
     
-    var contenido = this.ui.find("#contenido");
-    contenido.height(screen.height - 50);
-    var div_mapa = this.ui.find("#div_mapa");
+    var $content = this.ui.find("div:jqmData(role=content)");
+    $content.height (screen.height - 50);
+    
     var mapOptions = {
         zoom: 18,
         center: pos_obelisco, 
         mapTypeId: google.maps.MapTypeId.ROADMAP
     };
-    this.mapa = new google.maps.Map(div_mapa[0], mapOptions);
+    this.mapa = new google.maps.Map($content[0], mapOptions);
     google.maps.event.trigger(this.mapa, 'resize');
     
     this.transmisor = new TransmisorDePosicion({ usuario: this.o.usuario});
